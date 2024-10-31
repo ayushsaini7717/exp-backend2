1337c9a520ffda9b05489bf1f2c86b1b73424032
ayushsaini7717 <ayushsaini7717@gmail.com>
2024-10-31 21:04:57 +0530

    csp

--- middlewares/IsloggedIn.ts
+++ middlewares/IsloggedIn.ts
@@ -4,6 +4,25 @@ const prisma = new PrismaClient()
 import jwt from 'jsonwebtoken'
 
 
+import express from 'express';
+
+
+const app = express();
+
+// CSP Middleware
+const setCSPHeaders = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; script-src 'self' https://vercel.live; connect-src 'self' https://vercel.live"
+  );
+  next();
+};
+
+// Attach CSP middleware
+app.use(setCSPHeaders);
+
+
+
 export const IsloggedIn=(async (req:Request,res:Response,next:NextFunction)=>{
     try{
         let token:string=req.headers.authorization ?? "";
