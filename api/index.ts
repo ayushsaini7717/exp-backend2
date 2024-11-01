4e094b4ba2a3131aa1619a0c92348ce1c56db76f
ayushsaini7717 <ayushsaini7717@gmail.com>
2024-11-01 09:58:01 +0530

    uu

--- api/index.ts
+++ api/index.ts
@@ -5,8 +5,20 @@ import router from "../Routes/userRoute";
 import expense from '../Routes/expenseRoute';
 import cors from 'cors';
 
+
+import bodyParser from 'body-parser';
+import path from 'path';
+
+
 app.use(cors());
 
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.setHeader(
+      'Content-Security-Policy-Report-Only',
+      "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+    );
+    next();
+  });
 
 
 app.use('/api/v1/user',router);
