642f306e9fd9489b4cbc0dfd5a8d3333b055c7d6
ayushsaini7717 <ayushsaini7717@gmail.com>
2024-11-01 23:02:50 +0530

    cspmidd

--- api/index.ts
+++ api/index.ts
@@ -12,14 +12,22 @@ import path from 'path';
 
 app.use(cors());
 
-app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self' https://vercel.live; img-src 'self' data: https://expense-backend-xpxu.vercel.app;");
+// app.use((req, res, next) => {
+//   res.setHeader("Content-Security-Policy", "default-src 'none'; script-src 'self' https://vercel.live; img-src 'self' data: https://expense-backend-xpxu.vercel.app;");
+//   next();
+// });
+
+
+
+const cspMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src 'self' https://expense-backend-xpxu.vercel.app; script-src-elem 'self' https://vercel.live; connect-src 'self' https://vercel.live"
+  );
   next();
-});
+};
 
-
-
-
+app.use(cspMiddleware);
 
 
 app.use('/api/v1/user',router);
